07f98d247f436d627dfe1d325156e4c629937a3f
dev_azi <azi26.dev@gmail.com>
2026-08-02 12:11:06 +0800

    feat: add a new certifications data file with IT Specialist credentials

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -2,17 +2,22 @@ import type { Certification } from "./types";
 
 export const certifications: Certification[] = [
   {
-    title: "Information Technology Specialist in Databases",
+    title: "IT Specialist in Databases",
     issuer: "Certiport",
     href: "https://www.credly.com/badges/57314292-2da7-48c3-ac2f-92765a7b3a73/public_url",
   },
   {
-    title: "Information Technology Specialist in Networking",
+    title: "IT Specialist in Networking",
     issuer: "Certiport",
     href: "https://www.credly.com/badges/76b92ed5-201f-46ce-80f0-44f081f96075/public_url",
   },
   {
-    title: "Information Technology Specialist in HTML and CSS",
+    title: "IT Specialist in Network Security",
+    issuer: "Certiport",
+    href: "https://www.credly.com/badges/4aef257a-6820-45fb-a984-5d9dac9f14c9/public_url",
+  },
+  {
+    title: "IT Specialist in HTML and CSS",
     issuer: "Certiport",
     href: "https://www.credly.com/badges/c8b83b2b-e914-4e90-82e0-4f6745b764d9/public_url",
   },
